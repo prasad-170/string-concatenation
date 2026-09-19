4f71b5eab3d2a00240700a1776372d3feeb36257
prasad <prasadd1805@gmail.com>
2026-09-19 18:25:53 +0530

    string concatenation innj ava

---
+++ concatenation.java
@@ -0,0 +1,10 @@
+public class concatenation {
+    public static void main(String[] prasad) {
+        // concatenation
+        String fname = "prasad";
+        String lname = "darade";
+        String fullname = fname + " " + lname; // concatenation use +
+        System.out.println(fullname);
+
+    }
+}
